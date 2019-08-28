3f271506d160734da9d2cbdf1e6deb0c77c7ba55
Thomas Lauf <thomas.lauf@tngtech.com>
2019-08-28 23:10:56 +0200

    Add option for output format
    - csv is default (backwards compatibility)
    - print error, if format is not supported

--- python/holidata.py
+++ python/holidata.py
@@ -5,11 +5,12 @@
 Holidata - generate holidata files.
 
 Usage:
-  holidata.py (--year=<value>) (--locale=<value>)
+  holidata.py (--year=<value>) (--locale=<value>) [--output=<value>]
 
 Options:
     --year=<value>       Specify which year to generate data for.
     --locale=<value>     Specify the locale for which data should be generated.
+    --output=(csv|json)  Specify the output format (default: csv).
 
 Dependencies:
     pip3 install arrow docopt
@@ -26,6 +27,11 @@ if __name__ == '__main__':
 
     if class_candidates:
         country = class_candidates[0](int(args['--year']))
-        print(country.to_csv())
+        if args['--output'] == 'csv' or args['--output'] is None:
+            print(country.to_csv())
+        elif args['--output'] == 'json':
+            print(country.to_json())
+        else:
+            sys.exit("Unsupported output format: {}".format(args['--output']))
     else:
         sys.exit("No plugin found for locale: {}".format(args['--locale']))
