ea45a80ce15605917abbcec3e136fc585d0383b8
Thomas Lauf <thomas.lauf@tngtech.com>
2019-08-29 15:30:08 +0200

    Define regions for Patriots' Day

--- python/countries.py
+++ python/countries.py
@@ -51,8 +51,8 @@ class USA(Country):
     12-25: [NRF] Christmas Day
     3. monday in January:    [V] Birthday of Martin Luther King, Jr.
     3. monday in February:   [NV] Washington's Birthday
-    3. monday in April:      [NV] Patriot's day
-    1. last monday in May:   [NV] Memorial day
+    3. monday in April: [MA,ME] [NV] Patriots' Day
+    1. last monday in May:   [NV] Memorial Day
     1. monday in September:  [NV] Labor Day
     2. monday in October:    [NV] Columbus Day
     4. thursday in November: [NV] Thanksgiving Day
