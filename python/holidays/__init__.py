ea74059f19f8852e44005688ec65091c3d7d0872
Thomas Lauf <thomas.lauf@tngtech.com>
2019-09-30 21:37:17 +0200

    Add locale de-BE

--- python/holidays/__init__.py
+++ python/holidays/__init__.py
@@ -3,6 +3,7 @@ from .holidays import Locale
 __all__ = [
     "Locale",
     "de-AT",
+    "de-BE",
     "de-CH",
     "de-DE",
     "en-US",
---
+++ python/holidays/de-BE.py
@@ -0,0 +1,20 @@
+from .holidays import Locale
+
+
+class de_BE(Locale):
+    u"""
+    01-01: [NF] Neujahr
+    05-01: [NF] Tag der Arbeit
+    07-21: [NF] Nationalfeiertag
+    08-15: [NRF] Mariä Himmelfahrt
+    11-01: [NRF] Allerheiligen
+    11-11: [NF] Waffenstillstand
+    12-25: [NRF] Weihnacht
+    0 days after Easter: [NRV] Ostern
+    1 day after Easter: [NRV] Ostermontag
+    39 days after Easter: [NRV] Christi Himmelfahrt
+    49 days after Easter: [NRV] Pfingsten
+    50 days after Easter: [NRV] Pfingstmontag
+    """
+
+    locale = "de-BE"
