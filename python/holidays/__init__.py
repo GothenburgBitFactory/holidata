bbb0e501adf1a86bf71197ecf8ed3952014212c0
Thomas Lauf <thomas.lauf@tngtech.com>
2019-10-05 23:57:20 +0200

    Add locale ru-RU

--- python/holidays/__init__.py
+++ python/holidays/__init__.py
@@ -14,5 +14,6 @@ __all__ = [
     "hr-HR",
     "nb-NO",
     "nl-BE",
+    "ru-RU",
     "sk-SK",
 ]
---
+++ python/holidays/ru-RU.py
@@ -0,0 +1,20 @@
+from dateutil.easter import EASTER_ORTHODOX
+
+from .holidays import Locale
+
+
+class ru_RU(Locale):
+    u"""
+    01-01: [NF] Новый Год
+    01-07: [NRF] Рождество Христово
+    02-23: [NF] День защитника Отечества
+    03-08: [NF] Международный женский день
+    05-01: [NF] Праздник весны и труда
+    05-09: [NF] День Победы
+    06-12: [NF] День России
+    11-04: [NF] День народного единства
+    Easter: [NRV] Пасха
+    """
+
+    locale = "ru-RU"
+    easter_type = EASTER_ORTHODOX
