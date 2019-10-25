9593a859d2070d75dfac5a03e366c14258d08a8f
lldh <leonora.duarte@gmail.com>
2019-10-25 16:56:33 +0100

    Create pt-PT.py
    Corpo de Deus is 60 days after Easter.

---
+++ python/holidays/pt-PT.py
@@ -0,0 +1,24 @@
+from dateutil.easter import EASTER_WESTERN
+
+from .holidays import Locale
+
+class pt_PT(Locale):
+    u"""
+    01-01: [NF] Ano Novo
+    04-25: [NF] Dia da Liberdade
+    05-01: [NF] Dia do Trabalhador
+    06-10: [NF] Dia de Portugal
+    08-15: [NF] Assunção de Nossa Senhora
+    10-05: [NF] Implantação da República
+    11-01: [NF] Dia de Todos os Santos
+    12-01: [NF] Restauração da Independência
+    12-08: [NF] Imaculada Conceição
+    12-25: [NF] Natal
+    47 days before Easter: [NRV] Carnaval
+    2 days before Easter: [NRV] Sexta-feira Santa
+    60 days after Easter: [NRV] Corpo de Deus
+    Easter: [NRV] Páscoa
+    """
+
+    locale = "pt-PT"
+    easter_type = EASTER_WESTERN
