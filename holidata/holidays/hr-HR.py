253070b03749ddd19ce8318dc0e02b98e6772ce4
Saša Janiška <gour@atmarama.com>
2020-01-02 17:26:08 +0100

    update croatian holidays according to the law valid for >=2020

--- holidata/holidays/hr-HR.py
+++ holidata/holidays/hr-HR.py
@@ -7,18 +7,19 @@ from .holidays import Locale
 class hr_HR(Locale):
     """
     01-01: [NF] Nova Godina
-    01-06: [NRF] Bogojavljanje / Sveta tri kralja
+    01-06: [NRF] Bogojavljanje ili Sveta tri kralja
     05-01: [NF] Praznik rada
     06-22: [NF] Dan antifašističke borbe
     06-25: [NF] Dan državnosti
-    08-05: [NF] Dan domovinske zahvalnosti
+    08-05: [NF] Dan pobjede i domovinske zahvalnosti i Dan hrvatskih branitelja
     08-15: [NRF] Velika Gospa
     10-08: [NF] Dan neovisnosti
-    11-01: [NRF] Dan svih svetih
+    11-01: [NRF] Svi sveti
+    11-18: [NRF] Dan sjećanja na žrtve Domovinskog rata i Dan sjećanja na žrtvu Vukovara i Škabrnje
     12-25: [NRF] Božić
     12-26: [NRF] Sveti Stjepan
     Easter: [NRV] Uskrs
-    1 day after Easter: [NRV] Uskršnji ponedjeljak
+    1 day after Easter: [NRV] Uskrsni ponedjeljak
     60 days after Easter: [NRV] Tijelovo
     """
 
