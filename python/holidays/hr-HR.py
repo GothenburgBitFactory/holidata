bc3f365d05d60705f05317cc460eee31d0c1e50e
Saša Janiška <gour@atmarama.com>
2020-01-02 15:11:47 +0100

    update Croatian holidays for 2020.

--- python/holidays/hr-HR.py
+++ python/holidays/hr-HR.py
@@ -9,12 +9,13 @@ class hr_HR(Locale):
     01-01: [NF] Nova Godina
     01-06: [NRF] Bogojavljanje / Sveta tri kralja
     05-01: [NF] Praznik rada
+    05-30: [NF] Dan državnosti   
     06-22: [NF] Dan antifašističke borbe
-    06-25: [NF] Dan državnosti
     08-05: [NF] Dan domovinske zahvalnosti
     08-15: [NRF] Velika Gospa
     10-08: [NF] Dan neovisnosti
     11-01: [NRF] Dan svih svetih
+    11-18: [NF] Dan sjećanja na žrtve Domovinskog rata i Dan sjećanja na žrtvu Vukovara i Škabrnje
     12-25: [NRF] Božić
     12-26: [NRF] Sveti Stjepan
     Easter: [NRV] Uskrs
