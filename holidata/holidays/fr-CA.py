f34f653a306c8db796c9c10c9ee8f4fb6caf98ea
Ghyslain Leclerc <ghleclerc@gmail.com>
2020-04-18 21:37:26 -0400

    Capitalisation correction and modified two holidays
    As far as I can tell, the capitalisation is now correct.  The only one not with
    capitals is the "Premier lundi d'août" in fr-CA and as far as I could find, that
    is the correct case.
    
    I have removed Manitoba from family day, since I have found out it is not
    actually an official holiday in Manitoba.
    
    I have added Saskatchewan on the first Monday of August list, since although
    it is not the "Civic Holiday" of the other provinces, I have discovered it is
    Saskatchewan Day.

--- holidata/holidays/fr-CA.py
+++ holidata/holidays/fr-CA.py
@@ -18,6 +18,11 @@ from .holidays import Holiday, Locale
 #                           economic-growth-tourism-and-culture/paid-holidays
 #   https://www2.gnb.ca/content/dam/gnb/Departments/petl-epft/PDF/es/
 #                           FactSheets/PublicHolidaysVacation.pdf
+#   https://www.gov.mb.ca/labour/standards/doc,gen-holidays-after-april-30-07,
+#                           factsheet.html#q12
+#   https://www.saskatchewan.ca/business/employment-standards/
+#       vacations-holidays-leaves-and-absences/public-statutory-holidays/
+#                           list-of-saskatchewan-public-holidays
 #
 # Also those sites for some information
 #   https://www.officeholidays.com/holidays/canada/canada-remembrance-day
@@ -34,22 +39,23 @@ from .holidays import Holiday, Locale
 
 class fr_CA(Locale):
     """
-    01-01: [NF] Jour de l'an
+    01-01: [NF] Jour de l'An
     06-24: [QC] [F] Fête Nationale
     07-01: [NF] Fête du Canada
-    11-11: [AB,BC,NB,NL,NT] [F] Jour du souvenir
+    11-11: [AB,BC,NB,NL,NT] [F] Jour du Souvenir
     12-25: [NRF] Jour de Noël
     12-26: [NRF] Lendemain de Noël
     2 days before Easter: [NRV] Vendredi Saint
     1 day after Easter: [AB,PE,QC] [RV] Lundi de Pâques
-    3. monday in February: [AB,ON,SK,NB] [V] Fête de la famille.
-    3. monday in February: [MB] [V] Journée Louis Riel.
-    3. monday in February: [PE] [V] Fête des insulaires.
-    1. monday in August: [MB,NT,NU] [V] Premier lundi d'août.
-    1. monday in August: [AB] [V] Fête du patrimoine.
-    1. monday in August: [NS] [V] Jour de la Fondation.
-    1. monday in August: [NB] [V] Jour du Nouveau-Brunswick.
-    1. monday in September: [NV] Fête du travail
+    3. monday in February: [AB,ON,SK,NB] [V] Fête de la Famille
+    3. monday in February: [MB] [V] Journée Louis Riel
+    3. monday in February: [PE] [V] Fête des Insulaires
+    1. monday in August: [NT,NU] [V] Premier lundi d'août
+    1. monday in August: [AB] [V] Fête du Patrimoine
+    1. monday in August: [SK] [V] Fête de la Saskatchewan
+    1. monday in August: [NS] [V] Jour de la Fondation
+    1. monday in August: [NB] [V] Jour du Nouveau-Brunswick
+    1. monday in September: [NV] Fête du Travail
     2. monday in October: [AB,BC,MB,NL,ON,QC,SK,NT,NU,YT] [V] Jour de l'Action de grâce
     """
 
@@ -62,15 +68,13 @@ class fr_CA(Locale):
                 region='QC',
                 date=SmartDayArrow(self.year, 5, 25).shift_to_weekday(
                             'monday', order=1, reverse=True, including=False),
-                description="Journée nationale des patriotes",
+                description="Journée Nationale des Patriotes",
                 flags="V",
                 notes="")]
 
 
     def holiday_fete_de_la_reine_victoria(self):
-        days = []
-        for region in ['AB', 'BC', 'MB', 'NS', 'ON', 'SK', 'NT', 'NU', 'YT']:
-            days.append(Holiday(
+        return [Holiday(
                 locale=self.locale,
                 region=region,
                 date=SmartDayArrow(self.year, 5, 25).shift_to_weekday(
@@ -78,7 +82,7 @@ class fr_CA(Locale):
                 description="Fête de la Reine Victoria",
                 flags="V",
                 notes=""
-            ))
-
-        return days
+                ) for region in
+                ['AB', 'BC', 'MB', 'NS', 'ON', 'SK', 'NT', 'NU', 'YT']
+            ]
 
