eaafaf02c4634fe3f089cfdfa70b9ee784be0e1c
Brian Burnett <mail@hikerspath.com>
2020-06-16 16:08:18 -0400

    Additional Office Holidays

--- holidata/holidays/en-US.py
+++ holidata/holidays/en-US.py
@@ -9,6 +9,7 @@ class en_US(Locale):
     01-01: [NF] New Year's Day
     07-04: [NF] Independence Day
     11-11: [NF] Veterans Day
+    12-24: [RF] Christmas Eve
     12-25: [NRF] Christmas Day
     3. monday in January: [V] Birthday of Martin Luther King, Jr.
     3. monday in February: [NV] Washington's Birthday
@@ -17,6 +18,7 @@ class en_US(Locale):
     1. monday in September: [NV] Labor Day
     2. monday in October: [NV] Columbus Day
     4. thursday in November: [NV] Thanksgiving Day
+    4. friday in November: [V] Day after Thanksgiving
     """
 
     locale = "en-US"
--- holidata/holidays/es-US.py
+++ holidata/holidays/es-US.py
@@ -9,6 +9,7 @@ class es_US(Locale):
     01-01: [NF] Año Neuvo
     07-04: [NF] Día de la Independiencia
     11-11: [NF] Día de los Veteranos
+    12-24: [RF] Nochebuena
     12-25: [NRF] Navidad
     3. monday in January: [V] Cumpleaños de Martin Luther King, Jr.
     3. monday in February: [NV] Día del Presidente
@@ -17,6 +18,7 @@ class es_US(Locale):
     1. monday in September: [NV] Día del Trabajo
     2. monday in October: [NV] Día de Columbus
     4. thursday in November: [NV] Día de Acción de Gracias
+    4. friday in November: [V] Día después de Acción de Gracias
     """
 
     locale = "es-US"
