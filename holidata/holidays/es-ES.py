e8c69cea33c0c1f98a8ac25b51c9b312a83f26da
Vicente Jimenez Aguilar <googuy@gmail.com>
2020-06-20 11:02:03 +0200

    Add Andalucía (AN) region holidays
    Signed-off-by: Vicente Jimenez Aguilar <googuy@gmail.com>

--- holidata/holidays/es-ES.py
+++ holidata/holidays/es-ES.py
@@ -8,6 +8,7 @@ class es_ES(Locale):
     """
     01-01: [NF] Año Nuevo
     01-06: [NRF] Día de los Reyes
+    02-28: [AN] [F] Día de Andalucía
     05-01: [NF] Fiesta del Trabajo
     08-15: [NRF] Asunción de la Virgen
     10-12: [NF] Fiesta Nacional de España
@@ -17,6 +18,7 @@ class es_ES(Locale):
     12-24: [NRF] Noche Buena
     12-25: [NRF] Navidad
     12-31: [NF] Noche Vieja
+    3 days before Easter: [AN] [RV] Jueves Santo
     2 days before Easter: [NRV] Viernes Santo
     Easter: [NRV] Pascua
     """
