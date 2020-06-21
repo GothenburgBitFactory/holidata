09ff5486eb08afc8e018c1160c861d9152c0e59c
Vicente Jimenez Aguilar <googuy@gmail.com>
2020-06-22 00:17:50 +0200

    Update locale es-ES
    Remove non-official holidays 'Noche Vieja' and 'Noche Buena'
      It is customary to stop working early even at noon to let people have dinner with their families or friends but they are not official holidays.
    
    More correct official holidays names
    - Use 'Epifanía del Señor' instead of 'Día de Reyes' for holiday 01-06
    - Use 'Todos los Santos' indstead of 'Día de Todos los Santos' or 'Festividad de Todos los Santos' for holiday 11-01
    - Use explicit 'Día de la Constitución Española' for holiday 12-06 because in Spain there are also regional constitutions
    
    Add regional holidays from 'Calendario laboral'

--- holidata/holidays/es-ES.py
+++ holidata/holidays/es-ES.py
@@ -3,22 +3,58 @@ from dateutil.easter import EASTER_WESTERN
 
 from .holidays import Locale
 
+"""
+Information taken from government websites around 2020-06
+    https://administracion.gob.es/pag_Home/atencionCiudadana/calendarios/laboral.html
+    http://www.seg-social.es/wps/portal/wss/internet/CalendarioLaboral
+    2020: https://www.boe.es/eli/es/res/2019/10/03/(1)
+    2019: https://www.boe.es/eli/es/res/2018/10/16/(1)
+
+Regional governments
+    [AN] https://www.juntadeandalucia.es/temas/trabajar/relaciones/calendario.html
+
+Also those sites for some information
+    https://es.wikipedia.org/wiki/Calendario_laboral
+"""
 
 class es_ES(Locale):
     """
     01-01: [NF] Año Nuevo
-    01-06: [NRF] Día de los Reyes
+    01-06: [NRF] Epifanía del Señor
+    02-28: [AN] [F] Día de Andalucía
+    03-01: [IB] [F] Día de las Illes Balears
+    03-13: [ML] [F] Estatuto de Autonomía de la Ciudad de Melilla
+    03-19: [GA,MC,NC,PV,VC] [RF] San José
+    04-23: [AR] [RF] San Jorge / Día de Aragón
+    04-23: [CL] [F] Fiesta de Castilla y León
     05-01: [NF] Fiesta del Trabajo
+    05-02: [MD] [F] Fiesta de la Comunidad de Madrid
+    05-17: [GA] [F] Día de las Letras Gallegas
+    05-30: [CN] [F] Día de Canarias
+    05-31: [CM] [F] Día de Castilla-La Mancha
+    06-09: [MC] [F] Día de la Región de Murcia
+    06-09: [RI] [F] Día de La Rioja
+    06-24: [CT,VC] [RF] San Juan
+    07-25: [GA,PV] [RF] Santiago Apóstol / Día Nacional de Galicia
+    07-28: [CB] [F] Día de las Instituciones de Cantabria
     08-15: [NRF] Asunción de la Virgen
+    09-02: [CE] [F] Día de Ceuta
+    09-08: [AS] [F] Día de Asturias
+    09-08: [EX] [F] Día de Extremadura
+    09-11: [CT] [F] Fiesta Nacional de Cataluña
+    09-15: [CB] [RF] La Bien Aparecida
+    10-09: [VC] [F] Día de la Comunitat Valenciana
     10-12: [NF] Fiesta Nacional de España
-    11-01: [NRF] Dia de todos los Santos
-    12-06: [NF] Dia de la Constitución
+    11-01: [NRF] Todos los Santos
+    12-06: [NF] Día de la Constitución Española
     12-08: [NRF] Inmaculada Concepción
-    12-24: [NRF] Noche Buena
-    12-25: [NRF] Navidad
-    12-31: [NF] Noche Vieja
+    12-25: [NRF] Natividad del Señor
+    12-26: [CT,IB] [RF] San Esteban
+    3 days before Easter: [AN,AR,AS,CB,CE,CL,CM,CN,EX,GA,IB,MC,MD,ML,NC,PV,RI] [RV] Jueves Santo
     2 days before Easter: [NRV] Viernes Santo
     Easter: [NRV] Pascua
+    1 day after Easter: [CB,CM,CT,IB,NC,PV,RI,VC] [RV] Lunes de Pascua
+    60 days after Easter: [CM] [RV] Corpus Christi
     """
 
     locale = "es-ES"
--- tests/snapshots/snap_test_holidata/test_holidata_produces_holidays_for_locale_and_year[es_ES-2017] 1.py
+++ tests/snapshots/snap_test_holidata/test_holidata_produces_holidays_for_locale_and_year[es_ES-2017] 1.py
@@ -9,11 +9,211 @@
     },
     {
         'date': '2017-01-06',
-        'description': 'Día de los Reyes',
+        'description': 'Epifanía del Señor',
         'locale': 'es-ES',
         'notes': '',
         'region': '',
         'type': 'NRF'
+    },
+    {
+        'date': '2017-02-28',
+        'description': 'Día de Andalucía',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'AN',
+        'type': 'F'
+    },
+    {
+        'date': '2017-03-01',
+        'description': 'Día de las Illes Balears',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'IB',
+        'type': 'F'
+    },
+    {
+        'date': '2017-03-13',
+        'description': 'Estatuto de Autonomía de la Ciudad de Melilla',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'ML',
+        'type': 'F'
+    },
+    {
+        'date': '2017-03-19',
+        'description': 'San José',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'GA',
+        'type': 'RF'
+    },
+    {
+        'date': '2017-03-19',
+        'description': 'San José',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'MC',
+        'type': 'RF'
+    },
+    {
+        'date': '2017-03-19',
+        'description': 'San José',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'NC',
+        'type': 'RF'
+    },
+    {
+        'date': '2017-03-19',
+        'description': 'San José',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'PV',
+        'type': 'RF'
+    },
+    {
+        'date': '2017-03-19',
+        'description': 'San José',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'VC',
+        'type': 'RF'
+    },
+    {
+        'date': '2017-04-13',
+        'description': 'Jueves Santo',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'AN',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-13',
+        'description': 'Jueves Santo',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'AR',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-13',
+        'description': 'Jueves Santo',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'AS',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-13',
+        'description': 'Jueves Santo',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'CB',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-13',
+        'description': 'Jueves Santo',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'CE',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-13',
+        'description': 'Jueves Santo',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'CL',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-13',
+        'description': 'Jueves Santo',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'CM',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-13',
+        'description': 'Jueves Santo',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'CN',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-13',
+        'description': 'Jueves Santo',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'EX',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-13',
+        'description': 'Jueves Santo',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'GA',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-13',
+        'description': 'Jueves Santo',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'IB',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-13',
+        'description': 'Jueves Santo',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'MC',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-13',
+        'description': 'Jueves Santo',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'MD',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-13',
+        'description': 'Jueves Santo',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'ML',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-13',
+        'description': 'Jueves Santo',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'NC',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-13',
+        'description': 'Jueves Santo',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'PV',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-13',
+        'description': 'Jueves Santo',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'RI',
+        'type': 'RV'
     },
     {
         'date': '2017-04-14',
@@ -32,6 +232,86 @@
         'type': 'NRV'
     },
     {
+        'date': '2017-04-17',
+        'description': 'Lunes de Pascua',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'CB',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-17',
+        'description': 'Lunes de Pascua',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'CM',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-17',
+        'description': 'Lunes de Pascua',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'CT',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-17',
+        'description': 'Lunes de Pascua',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'IB',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-17',
+        'description': 'Lunes de Pascua',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'NC',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-17',
+        'description': 'Lunes de Pascua',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'PV',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-17',
+        'description': 'Lunes de Pascua',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'RI',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-17',
+        'description': 'Lunes de Pascua',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'VC',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-04-23',
+        'description': 'San Jorge / Día de Aragón',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'AR',
+        'type': 'RF'
+    },
+    {
+        'date': '2017-04-23',
+        'description': 'Fiesta de Castilla y León',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'CL',
+        'type': 'F'
+    },
+    {
         'date': '2017-05-01',
         'description': 'Fiesta del Trabajo',
         'locale': 'es-ES',
@@ -40,12 +320,156 @@
         'type': 'NF'
     },
     {
+        'date': '2017-05-02',
+        'description': 'Fiesta de la Comunidad de Madrid',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'MD',
+        'type': 'F'
+    },
+    {
+        'date': '2017-05-17',
+        'description': 'Día de las Letras Gallegas',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'GA',
+        'type': 'F'
+    },
+    {
+        'date': '2017-05-30',
+        'description': 'Día de Canarias',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'CN',
+        'type': 'F'
+    },
+    {
+        'date': '2017-05-31',
+        'description': 'Día de Castilla-La Mancha',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'CM',
+        'type': 'F'
+    },
+    {
+        'date': '2017-06-09',
+        'description': 'Día de la Región de Murcia',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'MC',
+        'type': 'F'
+    },
+    {
+        'date': '2017-06-09',
+        'description': 'Día de La Rioja',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'RI',
+        'type': 'F'
+    },
+    {
+        'date': '2017-06-15',
+        'description': 'Corpus Christi',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'CM',
+        'type': 'RV'
+    },
+    {
+        'date': '2017-06-24',
+        'description': 'San Juan',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'CT',
+        'type': 'RF'
+    },
+    {
+        'date': '2017-06-24',
+        'description': 'San Juan',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'VC',
+        'type': 'RF'
+    },
+    {
+        'date': '2017-07-25',
+        'description': 'Santiago Apóstol / Día Nacional de Galicia',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'GA',
+        'type': 'RF'
+    },
+    {
+        'date': '2017-07-25',
+        'description': 'Santiago Apóstol / Día Nacional de Galicia',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'PV',
+        'type': 'RF'
+    },
+    {
+        'date': '2017-07-28',
+        'description': 'Día de las Instituciones de Cantabria',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'CB',
+        'type': 'F'
+    },
+    {
         'date': '2017-08-15',
         'description': 'Asunción de la Virgen',
         'locale': 'es-ES',
         'notes': '',
         'region': '',
         'type': 'NRF'
+    },
+    {
+        'date': '2017-09-02',
+        'description': 'Día de Ceuta',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'CE',
+        'type': 'F'
+    },
+    {
+        'date': '2017-09-08',
+        'description': 'Día de Asturias',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'AS',
+        'type': 'F'
+    },
+    {
+        'date': '2017-09-08',
+        'description': 'Día de Extremadura',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'EX',
+        'type': 'F'
+    },
+    {
+        'date': '2017-09-11',
+        'description': 'Fiesta Nacional de Cataluña',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'CT',
+        'type': 'F'
+    },
+    {
+        'date': '2017-09-15',
+        'description': 'La Bien Aparecida',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'CB',
+        'type': 'RF'
+    },
+    {
+        'date': '2017-10-09',
+        'description': 'Día de la Comunitat Valenciana',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'VC',
+        'type': 'F'
     },
     {
         'date': '2017-10-12',
@@ -57,7 +481,7 @@
     },
     {
         'date': '2017-11-01',
-        'description': 'Dia de todos los Santos',
+        'description': 'Todos los Santos',
         'locale': 'es-ES',
         'notes': '',
         'region': '',
@@ -65,7 +489,7 @@
     },
     {
         'date': '2017-12-06',
-        'description': 'Dia de la Constitución',
+        'description': 'Día de la Constitución Española',
         'locale': 'es-ES',
         'notes': '',
         'region': '',
@@ -80,27 +504,27 @@
         'type': 'NRF'
     },
     {
-        'date': '2017-12-24',
-        'description': 'Noche Buena',
-        'locale': 'es-ES',
-        'notes': '',
-        'region': '',
-        'type': 'NRF'
-    },
-    {
         'date': '2017-12-25',
-        'description': 'Navidad',
+        'description': 'Natividad del Señor',
         'locale': 'es-ES',
         'notes': '',
         'region': '',
         'type': 'NRF'
     },
     {
-        'date': '2017-12-31',
-        'description': 'Noche Vieja',
+        'date': '2017-12-26',
+        'description': 'San Esteban',
         'locale': 'es-ES',
         'notes': '',
-        'region': '',
-        'type': 'NF'
+        'region': 'CT',
+        'type': 'RF'
+    },
+    {
+        'date': '2017-12-26',
+        'description': 'San Esteban',
+        'locale': 'es-ES',
+        'notes': '',
+        'region': 'IB',
+        'type': 'RF'
     }
 ]
