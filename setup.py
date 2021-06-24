4803d18d23135c60eaac6546e43c5166f50d6576
Thomas Lauf <thomas.lauf@tngtech.com>
2021-06-24 21:21:57 +0200

    Restructure setup.py
    - Add sub-package 'holidata/holidays'
    - Split 'requires' into 'install_requires' and 'extras_require'
    
    Signed-off-by: Thomas Lauf <thomas.lauf@tngtech.com>

--- setup.py
+++ setup.py
@@ -1,16 +1,51 @@
-from distutils.core import setup
+#!/usr/bin/env python3
 
-config = {
-    'name': 'holidata',
-    'version': '2020.1',
-    'packages': ['holidata', 'holidata.holidays'],
-    'license': 'MIT License',
-    'description': 'Holidata is a utility for algorithmically producing holidays for a given locale and year',
-    'url': 'https://github.com/GothenburgBitFactory/holidata',
-    'author': 'Gothenburg Bit Factory',
-    'author_email': 'support@gothenburgbitfactory.org',
-    'requires': ['dateutils', 'docopt', 'arrow', 'snapshottest', 'pytest'],
-    'scripts': ['bin/holidata'],
-}
+from setuptools import find_packages, setup
+import pathlib
 
-setup(**config)
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="holidata",
+    version="2022.7.0",
+    description="Holidata is a utility for algorithmically producing holidays for a given locale and year",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GothenburgBitFactory/holidata",
+    author="Gothenburg Bit Factory",
+    author_email="support@gothenburgbitfactory.org",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords="holiday, calendar",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    include_package_data=True,
+    python_requires=">=3.7, <4",
+    install_requires=[
+        "arrow >= 1.2.2",
+        "docopt >= 0.6.2",
+        "python-dateutil >= 2.8.2",
+    ],
+    extras_require={
+        "test": [
+            "pytest >= 7.1.2",
+            "snapshottest >= 0.6.0",
+        ]
+    },
+    scripts=["bin/holidata"],
+    project_urls={
+        "Bug Reports": "https://github.com/GothenburgBitFactory/holidata/issues",
+        "Source": "https://github.com/GothenburgBitFactory/holidata",
+        "Website": "https://holidata.net",
+    },
+)
