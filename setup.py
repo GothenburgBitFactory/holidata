9c472ef190076a61ecc0efc3814668b10b5a06f2
Tomas Babej <tomas@tbabej.com>
2021-10-01 22:07:13 -0400

    setup: Make sure holidata.holidays package is also installed
    Signed-off-by: Tomas Babej <tomas@tbabej.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 config = {
     'name': 'holidata',
     'version': '2020.1',
-    'packages': ['holidata'],
+    'packages': ['holidata', 'holidata.holidays'],
     'license': 'MIT License',
     'description': 'Holidata is a utility for algorithmically producing holidays for a given locale and year',
     'url': 'https://github.com/GothenburgBitFactory/holidata',
