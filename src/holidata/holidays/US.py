192093519e9c1cd3a61e3d5af8b5661dadf4820c
Thomas Lauf <thomas.lauf@tngtech.com>
2024-11-12 21:43:16 +0100

    Implement US federal holidays
    As defined in federal law (5 U.S.C. 6103) according to https://www.opm.gov/policy-data-oversight/pay-leave/federal-holidays/
    
    Closes #114
    
    Signed-off-by: Thomas Lauf <thomas.lauf@tngtech.com>

--- src/holidata/holidays/US.py
+++ src/holidata/holidays/US.py
@@ -1,7 +1,7 @@
 from dateutil.easter import EASTER_WESTERN
 
 from holidata.holiday import Country
-from holidata.utils import first, third, last, second, fourth, day
+from holidata.utils import SmartDayArrow, first, second, third, fourth, last, day
 
 
 class US(Country):
@@ -23,10 +23,75 @@ class US(Country):
 
         self.define_holiday() \
             .with_names({
+                "en": "New Year's Day (in lieu of)",
+                "es": "Año Neuvo (en lugar de)",
+            }) \
+            .on(month=12, day=31) \
+            .on_condition(US.date_is_friday(month=12, day=31)) \
+            .with_flags("NF")
+
+        self.define_holiday() \
+            .with_names({
+                "en": "New Year's Day (in lieu of)",
+                "es": "Año Neuvo (en lugar de)",
+            }) \
+            .on(month=1, day=2) \
+            .on_condition(US.date_is_sunday(month=1, day=1)) \
+            .with_flags("NF")
+
+        self.define_holiday() \
+            .with_names({
+                "en": "Juneteenth National Independence Day",
+                "es": "Juneteenth – Día de la Emancipación",
+            }) \
+            .since(2021) \
+            .on(month=6, day=19) \
+            .with_flags("NF")
+
+        self.define_holiday() \
+            .with_names({
+                "en": "Juneteenth National Independence Day (in lieu of)",
+                "es": "Juneteenth – Día de la Emancipación (en lugar de)",
+            }) \
+            .since(2021) \
+            .on(month=6, day=18) \
+            .on_condition(US.date_is_saturday(month=6, day=19)) \
+            .with_flags("NF")
+
+        self.define_holiday() \
+            .with_names({
+                "en": "Juneteenth National Independence Day (in lieu of)",
+                "es": "Juneteenth – Día de la Emancipación (en lugar de)",
+            }) \
+            .since(2021) \
+            .on(month=6, day=20) \
+            .on_condition(US.date_is_sunday(month=6, day=19)) \
+            .with_flags("NF")
+
+        self.define_holiday() \
+            .with_names({
                 "en": "Independence Day",
                 "es": "Día de la Independiencia",
             }) \
             .on(month=7, day=4) \
+            .with_flags("NF")
+
+        self.define_holiday() \
+            .with_names({
+                "en": "Independence Day (in lieu of)",
+                "es": "Día de la Independiencia (en lugar de)",
+            }) \
+            .on(month=7, day=3) \
+            .on_condition(US.date_is_saturday(month=7, day=4)) \
+            .with_flags("NF")
+
+        self.define_holiday() \
+            .with_names({
+                "en": "Independence Day (in lieu of)",
+                "es": "Día de la Independiencia (en lugar de)",
+            }) \
+            .on(month=7, day=5) \
+            .on_condition(US.date_is_sunday(month=7, day=4)) \
             .with_flags("NF")
 
         self.define_holiday() \
@@ -39,10 +104,29 @@ class US(Country):
 
         self.define_holiday() \
             .with_names({
+                "en": "Veterans Day (in lieu of)",
+                "es": "Día de los Veteranos (en lugar de)",
+            }) \
+            .on(month=11, day=10) \
+            .on_condition(US.date_is_saturday(month=11, day=11)) \
+            .with_flags("NF")
+
+        self.define_holiday() \
+            .with_names({
+                "en": "Veterans Day (in lieu of)",
+                "es": "Día de los Veteranos (en lugar de)",
+            }) \
+            .on(month=11, day=12) \
+            .on_condition(US.date_is_sunday(month=11, day=11)) \
+            .with_flags("NF")
+
+        self.define_holiday() \
+            .with_names({
                 "en": "Christmas Eve",
                 "es": "Nochebuena",
             }) \
             .on(month=12, day=24) \
+            .on_condition(US.date_is_not_saturday(month=12, day=25)) \
             .with_flags("NRF")
 
         self.define_holiday() \
@@ -51,6 +135,24 @@ class US(Country):
                 "es": "Navidad",
             }) \
             .on(month=12, day=25) \
+            .with_flags("NRF")
+
+        self.define_holiday() \
+            .with_names({
+                "en": "Christmas Day (in lieu of)",
+                "es": "Navidad (en lugar de)",
+            }) \
+            .on(month=12, day=24) \
+            .on_condition(US.date_is_saturday(month=12, day=25)) \
+            .with_flags("NRF")
+
+        self.define_holiday() \
+            .with_names({
+                "en": "Christmas Day (in lieu of)",
+                "es": "Navidad (en lugar de)",
+            }) \
+            .on(month=12, day=26) \
+            .on_condition(US.date_is_sunday(month=12, day=25)) \
             .with_flags("NRF")
 
         self.define_holiday() \
@@ -117,3 +219,31 @@ class US(Country):
             }) \
             .on(day(1).after(fourth("thursday").of("november"))) \
             .with_flags("NV")
+
+    @staticmethod
+    def date_is_not_saturday(month, day):
+        def wrapper(year):
+            return SmartDayArrow(year, month, day).weekday() != "saturday"
+
+        return wrapper
+
+    @staticmethod
+    def date_is_friday(month, day):
+        def wrapper(year):
+            return SmartDayArrow(year, month, day).weekday() == "friday"
+
+        return wrapper
+
+    @staticmethod
+    def date_is_saturday(month, day):
+        def wrapper(year):
+            return SmartDayArrow(year, month, day).weekday() == "saturday"
+
+        return wrapper
+
+    @staticmethod
+    def date_is_sunday(month, day):
+        def wrapper(year):
+            return SmartDayArrow(year, month, day).weekday() == "sunday"
+
+        return wrapper
