1223f700be9cb25e23e45be2a43ca3a0d234c664
Thomas Lauf <thomas.lauf@tngtech.com>
2025-01-20 21:47:16 +0100

    Add holiday "Fronleichnam" to region TH (Thüringen) in locale DE
    Only in communities with a catholic majority
    
    Signed-off-by: Thomas Lauf <thomas.lauf@tngtech.com>

--- src/holidata/holidays/DE/TH.py
+++ src/holidata/holidays/DE/TH.py
@@ -1,4 +1,5 @@
 from holidata.holiday import Region
+from holidata.utils import day
 
 
 class TH(Region):
@@ -22,3 +23,9 @@ class TH(Region):
             .since(2018) \
             .on(month=10, day=31) \
             .with_flags("RF")
+
+        self.define_holiday() \
+            .with_name("Fronleichnam") \
+            .on(day(60).after(country.easter())) \
+            .with_flags("RV") \
+            .annotated_with("In Gemeinden mit überwiegend katholischer Bevölkerung")
