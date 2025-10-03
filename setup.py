1a957416d776b248ce2ea32ce16e9b82ac2da2f7
Thomas Lauf <thomas.lauf@tngtech.com>
2025-10-03 21:32:32 +0200

    Drop support for Python 3.9
    Signed-off-by: Thomas Lauf <thomas.lauf@tngtech.com>

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
@@ -32,7 +31,7 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     include_package_data=True,
-    python_requires=">=3.9, <4",
+    python_requires=">=3.10, <4",
     install_requires=[
         "arrow >= 1.3.0",
         "docopt >= 0.6.2",
