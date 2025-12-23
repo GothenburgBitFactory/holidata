48d2614697d28f0a50993275909e0ea7b29edd71
Thomas Lauf <thomas.lauf@tngtech.com>
2025-12-23 17:06:25 +0100

    Adding the new holiday 'Festa nazionale di San Francesco d'Assisi, patrono d'Italia'
    Also updating the Italian holidays and aligning them with their sources.
    - Added 'Santi Apostoli Pietro e Paolo' for the municipality of Rome
    - Added 'Unità nazionale' (celebrated on the first Sunday in November)
    
    Signed-off-by: Thomas Lauf <thomas.lauf@tngtech.com>

--- src/holidata/holidays/IT/__init__.py
+++ src/holidata/holidays/IT/__init__.py
@@ -1,11 +1,21 @@
 from dateutil.easter import EASTER_WESTERN
 
 from holidata.holiday import Country
-from holidata.utils import day
+from holidata.utils import day, first
 
 __all__ = [
     "IT",
 ]
+
+"""
+sources:
+- LEGGE 27 maggio   1949, n. 260 https://www.gazzettaufficiale.it/eli/gu/1949/05/31/124/sg
+- LEGGE  5 marzo    1977, n.  54 https://www.gazzettaufficiale.it/eli/id/1977/03/07/077U0054/sg 
+- LEGGE 20 novembre 2000, n. 336 https://www.gazzettaufficiale.it/eli/id/2000/11/22/000G0390/sg
+- DECRETO DEL PRESIDENTE DELLA REPUBBLICA 28 dicembre 1985, n. 792
+                                 https://www.gazzettaufficiale.it/eli/id/1985/12/31/085U0792/sg
+- LEGGE  8 ottobre  2025, n. 151 https://www.gazzettaufficiale.it/eli/id/2025/10/10/25G00153/sg
+"""
 
 
 class IT(Country):
@@ -43,14 +53,31 @@ class IT(Country):
             .with_flags("NF")
 
         self.define_holiday() \
+            .with_name("Santi Apostoli Pietro e Paolo") \
+            .on(month=6, day=29) \
+            .with_flags("NRF") \
+            .annotated_with("Solo per il comune di Roma")
+
+        self.define_holiday() \
             .with_name("Assunzione (ferragosto)") \
             .on(month=8, day=15) \
+            .with_flags("NRF")
+
+        self.define_holiday() \
+            .with_name("Festa nazionale di San Francesco d'Assisi, patrono d'Italia") \
+            .since(2026) \
+            .on(month=10, day=4) \
             .with_flags("NRF")
 
         self.define_holiday() \
             .with_name("Ognissanti") \
             .on(month=11, day=1) \
             .with_flags("NRF")
+
+        self.define_holiday() \
+            .with_name("Unità nazionale") \
+            .on(first("sunday").of("november")) \
+            .with_flags("NF")
 
         self.define_holiday() \
             .with_name("Immacolata concezione") \
