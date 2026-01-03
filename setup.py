321a7ee0fbe60da2fa926f3f5615322921128634
Thomas Lauf <thomas.lauf@tngtech.com>
2026-01-03 16:52:00 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="holidata",
-    version="2026.1.0",
+    version="2026.1.1",
     description="Holidata is a utility for algorithmically producing holidays for a given locale and year",
     long_description=long_description,
     long_description_content_type="text/markdown",
